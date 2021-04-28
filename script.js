60c758af06c08261257ad976b337082e663175bf
Alexis Ligon <aligon17@gmail.com>
2021-04-28 13:28:13 -0400

    added comments

--- script.js
+++ script.js
@@ -8,22 +8,23 @@ function displayTime() {
 setInterval(displayTime, 1000);
 
 //--------------------------
+//array for work hours in the day
 var workHours = ['9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm']
 
-
+//function to display table
 function displayHours (hour) {
-  var hourRow = $('<tr>');
+  var hourRow = $('<tr>');//creat a table row
 
-  var hourDisplay = $('<td>').addClass('p-2').text('hour');
+  var hourDisplay = $('<td>').addClass('p-2').text('hour');//create a data cell to display the hour
 
-  var todoBox = $('<td>').addClass('p-2');
-  var todoInput = $('<textarea>');
-  todoBox.append(todoInput);
+  var todoBox = $('<td>').addClass('p-2');//create a data cell to write out todos
+  var todoInput = $('<textarea>');//creates text area for input
+  todoBox.append(todoInput);//inserts a text area box to the data cell
 
-  var saveBox = $('<td>').addClass('p-2');
-  var saveBtn = $('<button>').addClass('btn btn-primary btn-lg saveBtn').text('save').attr('data-time', hour);
+  var saveBox = $('<td>').addClass('p-2');//create a data cell for save button
+  var saveBtn = $('<button>').addClass('btn btn-primary btn-lg saveBtn').text('save').attr('data-time', hour);//creates the button
   
-  saveBox.append(saveBtn);
+  saveBox.append(saveBtn);//inserts a button into the box
 
   var nextHour = moment().format('hh a');
   console.log(nextHour);
@@ -31,12 +32,13 @@ function displayHours (hour) {
   hourDisplay.text(hour);
   console.log(hourDisplay);
 
-$('#todo-display').append(hourRow);
-  hourRow.append(hourDisplay);
-  hourRow.append(todoBox);
-hourRow.append(saveBox);
+$('#todo-display').append(hourRow);//appends the row to the table
+  hourRow.append(hourDisplay);//appends the hour to the row
+  hourRow.append(todoBox);//appends the textbox to the row
+hourRow.append(saveBox);//appends the save box to the row
 }
 
+//displays a new row for each hour in the array
 for (var i = 0; i < workHours.length; i++) {
   displayHours(workHours[i]);
 }
